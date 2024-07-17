6d1dc519491f80a20e1600b135c869154bca40fe
Murad Adigozalov <83539144+murad9911@users.noreply.github.com>
2024-07-17 15:47:01 +0400

    Create config.py

---
+++ ad/config.py
@@ -0,0 +1,18 @@
+# config.py
+
+# Telegram bot token
+TOKEN = ''
+
+# Active Directory credentials
+AD_SERVER = ''
+AD_USERNAME = ''
+AD_PASSWORD = ''
+BASE_DN = ''
+
+# Authorization token for SMS API
+SMS_API_TOKEN = ''
+SMS_URL = ''      # SMS api url
+SMS_SENDER = ''   # SMS sender name
+
+# File to store SMS limits
+SMS_LIMIT_FILE = 'sms_limits.json'
